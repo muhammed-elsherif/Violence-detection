ef04f89c9f3c6fbc58cec69b35330227fcaf93fe
Muhammed Nasser <mo.nasser20032003@gmail.com>
2025-04-26 04:03:18 +0300

    implement user statistics display and service integration

--- frontend/src/app/components/users/users.component.ts
+++ frontend/src/app/components/users/users.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from "@angular/core";
+import { UserAdminService } from "../../core/services/user-admin.service";
+import { DatePipe } from "@angular/common";
 
 @Component({
-  selector: 'app-users',
-  imports: [],
-  templateUrl: './users.component.html',
-  styleUrl: './users.component.scss'
+  selector: "app-users",
+  standalone: true,
+  imports: [DatePipe],
+  templateUrl: "./users.component.html",
+  styleUrl: "./users.component.scss",
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  userStats: any[] = [];
 
+  userAdminService = inject(UserAdminService);
+
+  ngOnInit(): void {
+    this.uploadUserStats();
+  }
+
+  uploadUserStats() {
+    this.userAdminService.getUserStats().subscribe({
+      next: (response) => {
+        console.log("Upload success:", response);
+        this.userStats = response;
+      },
+      error: (err) => console.error("Upload error:", err),
+    });
+  }
 }
