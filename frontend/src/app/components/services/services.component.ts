6faee029612f74d72abdfb89402534cde8cb2ce2
mohamed-abdelrazak <zoka1080p@gmail.com>
2025-05-09 21:13:55 +0300

    added services page

--- frontend/src/app/components/services/services.component.ts
+++ frontend/src/app/components/services/services.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-services',
-  imports: [],
-  templateUrl: './services.component.html',
-  styleUrl: './services.component.scss'
+  selector: "app-services",
+  imports: [RouterLink],
+  templateUrl: "./services.component.html",
+  styleUrl: "./services.component.scss",
 })
 export class ServicesComponent {
+  searchQuery: string = "";
 
+  models = [
+    {
+      id: 1,
+      name: "Fire Detection",
+      type: "CV",
+      description: "Detect fire incidents in real-time video feed.",
+      endpoint: "/api/models/fire-detection",
+    },
+    {
+      id: 2,
+      name: "Object Tracking",
+      type: "CV",
+      description: "Track the movement of objects in a video.",
+      endpoint: "/api/models/object-tracking",
+    },
+  ];
 }
