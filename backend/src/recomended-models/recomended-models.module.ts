e1014911ca926b7188f7eeb6958dc657e3a78f54
Mohamed Mostafa <91856839+muhammed-elsherif@users.noreply.github.com>
2025-05-26 02:34:28 +0300

    Add recommended model service and controller (#12)
    * Add recommended model service and controller
    
    * Add recommended model fix
    
    * Add recommended model fix1
    
    ---------
    
    Co-authored-by: Youssef Nady <youssefmohamednady@gmail.com>

---
+++ backend/src/recomended-models/recomended-models.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { HttpModule } from "@nestjs/axios";
+import { RecomendedModelsController } from "./recomended-models.controller";
+import { RecomendedModelsService } from "./recomended-models.service";
+
+@Module({
+  imports: [HttpModule],
+  controllers: [RecomendedModelsController],
+  providers: [RecomendedModelsService],
+})
+export class RecomendedModelsModule {}
---
+++ backend/src/recomended-models/recommendation.dto.ts
@@ -0,0 +1,12 @@
+import { IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class RecommendationDto {
+  @ApiProperty({ example: "Salah for transportion" })
+  @IsString()
+  company_name: string;
+
+  @ApiProperty({ example: "We are a big company in transportation and storing goods" })
+  @IsString()
+  use_case: string;
+}
