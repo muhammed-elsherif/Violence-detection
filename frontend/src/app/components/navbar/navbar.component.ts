8aa7560471a56b0656505e2b8f9b19f96dde6e6d
mohamedelsheriff <mohamed.elsherif@incorta.com>
2025-05-25 21:54:36 +0300

    fixed auth

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -9,10 +9,15 @@ import { RouterLink, RouterLinkActive, Router } from "@angular/router";
 })
 export class NavbarComponent {
   private readonly Router = inject(Router);
+  isLoggedIn: boolean = localStorage.getItem("access_token") ? true : false;
 
-  onSubmit(): void {
+  onLogout(): void {
     localStorage.removeItem("access_token");
     localStorage.removeItem("refresh_token");
+    this.Router.navigate(["/login"]);
+  }
+
+  onLogin(): void {
     this.Router.navigate(["/login"]);
   }
 }
