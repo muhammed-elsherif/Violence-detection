a157f725c633e7829924e5cb444f67acf850109c
mohamed-abdelrazak <zoka1080p@gmail.com>
2025-05-27 15:11:55 +0300

    Added footer

--- frontend/src/app/layouts/user/user.component.ts
+++ frontend/src/app/layouts/user/user.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-user',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
