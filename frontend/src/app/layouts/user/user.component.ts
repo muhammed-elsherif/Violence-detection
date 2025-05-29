510bec41968ed8964951eff6202bc50647811581
Muhammed Nasser <mo.nasser20032003@gmail.com>
2025-05-29 06:04:37 +0300

    update routing

--- frontend/src/app/layouts/user/user.component.ts
+++ frontend/src/app/layouts/user/user.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
-  selector: 'app-user',
+  selector: "app-user",
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  templateUrl: "./user.component.html",
+  styleUrl: "./user.component.scss",
 })
 export class UserComponent {
+  private readonly _Router = inject(Router);
 
+  openCart() {
+    this._Router.navigate(["/purchase-model"]);
+  }
 }
